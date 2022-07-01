e9724b25d8764185dc3cfe13f8559eddefbece8b
Hennadii <82006228+HennadiiZ@users.noreply.github.com>
2022-07-01 16:37:40 +0300

    Annotations and Variables + Object Literal Annotations

--- annotations/appendix-typescript.ts
+++ annotations/appendix-typescript.ts
@@ -2,3 +2,5 @@
 
 // 357. Type Annotations and Inference
 // 358. Annotations and Variables
+// 359. Object Literal Annotations
+// 
--- annotations/variables.ts
+++ annotations/variables.ts
@@ -1,3 +1,4 @@
+// 358. Annotations and Variables
 const apples: number = 5;
 let speed: string = "fast";
 let hasName: boolean = false;
@@ -5,4 +6,26 @@ let hasName: boolean = false;
 let nothing: null = null;
 let nthng: undefined = undefined;
 
+// built in objects
 let now: Date = new Date();
+
+// 359. Object Literal Annotations
+//Array 
+let colors: string[] = ['red', 'green', 'blue',]; // arr of strings
+let colorsMore: Array<string> = ['red', 'green', 'blue',];
+
+let numbers: number[] = [1, 4]; // arr of numbers
+let numbersMore: Array<number> = [1, 4];
+
+let truths: boolean[] = [ true, false];
+let truthsMore: Array<boolean> = [ true, false];
+
+// Classes
+class Car {
+
+}
+let car: Car = new Car();
+
+//Object literal 
+let point: { x: number; y: number } = { x: 0, y: 0 };
+let pointStr: { x: string; y: string  } = { x: "hey", y: "hoy" };
