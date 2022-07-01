36241a6621e8020fd120b487409c9a3a232a6840
Hennadii <82006228+HennadiiZ@users.noreply.github.com>
2022-07-01 10:17:22 +0300

    // ======= //  Section 25: Appendix - TypeScript

---
+++ annotations/appendix-typescript.ts
@@ -0,0 +1,4 @@
+// ======= //  Section 25: Appendix - TypeScript
+
+// 357. Type Annotations and Inference
+// 358. Annotations and Variables
---
+++ annotations/variables.ts
@@ -0,0 +1,8 @@
+const apples: number = 5;
+let speed: string = "fast";
+let hasName: boolean = false;
+
+let nothing: null = null;
+let nthng: undefined = undefined;
+
+let now: Date = new Date();
